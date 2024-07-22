c5e9ada93af043a5b6cb9eba49a87212ed5fc4b5
Anand kumar <anandmickey0187@gmail.com>
2024-07-22 11:50:21 +0530

    image uploading bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import fs from "fs"
 
 const registerUser = asyncHandler(async (req,res) => {
     // get all data from user
@@ -26,10 +27,15 @@ const registerUser = asyncHandler(async (req,res) => {
         $or: [{ userName }, { email }]
     })
     if(existedUser){
+        fs.unlinkSync()
         throw new ApiError(409, "User with userName or email already exists");
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required");
