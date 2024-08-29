ea102bfaa6d7784288e354f09290903d880ed0e0
Anand kumar <anandmickey0187@gmail.com>
2024-08-29 17:57:06 +0530

    add getWatchHistory end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -290,6 +290,120 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,user, "Cover image is updated successfully"));
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const { userName } = req.params;
+
+    if(!userName.trim()){
+        throw new ApiError(400, "username is missing")
+    };
+
+    const channel = await User.aggregate([
+        {
+            $match : {
+                userName : userName?.toLowerCase()
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as : "subscribedTo"
+            }
+        },
+        {
+            $addFields : {
+                subscibersCount : {
+                    $size : "$subscribers"
+                },
+                ChannelSubscribedToCount: {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed : {
+                    $cond: {
+                        if : {$in : [req?.user?._id, "subscribers.subscriber"]},
+                        then : true,
+                        else : false,
+                    }
+                }
+            }
+        },
+        {
+            $project : {
+                fullName : 1,
+                userName: 1,
+                email : 1,
+                isSubscribed: 1,
+                ChannelSubscribedToCount: 1,
+                subscibersCount: 1,
+                avatar : 1,
+                coverImage : 1
+            }
+        }
+    ]);
+
+    if(!channel?.length){
+        throw new ApiError(400, "Channel doesn't exist");
+    }
+
+    return res.status(200).json(new ApiResponse(200, channel[0], "User channel fetched successfully"));
+})
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+
+    const user = await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField : "watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                pipeline : [
+                    {
+                        $lookup : {
+                            from : "users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as: "owner",
+                            pipeline : [
+                                {
+                                    $project: {
+                                        fullName : 1,
+                                        userName : 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields : {
+                            owner : {
+                                $first : "$owner"
+                            }
+                        }
+                    },
+                ]
+            }
+        }
+    ]);
+
+    return res.status(200).json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"));
+})
+
 export { 
     registerUser,
     loginUser,
@@ -299,5 +413,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
  };
